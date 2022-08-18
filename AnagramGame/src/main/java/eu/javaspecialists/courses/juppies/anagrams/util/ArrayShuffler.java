0956c91138b510f08936e9d6812173db68290241
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2022-08-18 17:02:13 +0300

    swap ints with XOR

--- AnagramGame/src/main/java/eu/javaspecialists/courses/juppies/anagrams/util/ArrayShuffler.java
+++ AnagramGame/src/main/java/eu/javaspecialists/courses/juppies/anagrams/util/ArrayShuffler.java
@@ -5,12 +5,12 @@ import java.util.*;
 public class ArrayShuffler {
     public void shuffle(int[] values) {
         Random random = new Random();
-        for (int i = values.length - 1; i > 0 ; i--) {
-            int swap = random.nextInt(i + 1) ;
-            if (swap !=i) {
-                values[i] += values[swap];
-                values[swap] = values[i] - values[swap];
-                values[i] -= values[swap];
+        for (int i = values.length - 1; i > 0; i--) {
+            int swap = random.nextInt(i + 1);
+            if (swap != i) {
+                values[i] ^= values[swap]; // violates POLA
+                values[swap] ^= values[i];
+                values[i] ^= values[swap];
             }
         }
     }
